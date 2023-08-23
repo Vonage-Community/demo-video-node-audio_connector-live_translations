984944806f166b1b1f2bbd11d930a7cf969c7ed9
Dwane Hemmings <dwane.hemmings@vonage.com>
2023-08-22 21:27:41 -0400

    Add files via upload

---
+++ credentials.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // You can find the API key and API secret in the dashboard: https://tokbox.com/account/
+  VONAGE_API_KEY: "",
+  VONAGE_API_SECRET: "",
+
+  // The URL of the running application without "https://". This must be a publically available URL on the internet.
+  APP_DOMAIN: "",
+
+  // Replace with your own subscription key, service region (e.g., "westus"),
+  // and recognition language. Can be found in the dashboard: https://portal.azure.com/
+  AZURE_SUBSCRIPTION_KEY: "",
+  AZURE_SERVICE_REGION: "", // e.g., "westus"
+  DEFAULT_TARGET_LANGUAGE: "", // view full list here: https://learn.microsoft.com/en-us/azure/ai-services/speech-service/language-support?tabs=speech-translation
+};
